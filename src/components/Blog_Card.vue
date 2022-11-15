b7111400fac60279ceb29962d3125aae4ff7dc57
shiyue <2422645737@qq.com>
2022-11-15 14:40:48 +0800

    2022-11-14

--- src/components/Blog_Card.vue
+++ src/components/Blog_Card.vue
@@ -1,8 +1,6 @@
 <template>
 <div>
-    <div class="title">
-        {{article.title}}
-    </div>
+    <router-link to="read" target="_blank">{{article.title}}</router-link>
     <hr>
     <div class="tag" v-show="article.type == 'blog'">
         <!-- 文章标签 -->
@@ -10,8 +8,8 @@
     </div>
     <div class="cover">
         <!-- 封面 -->
-        <img v-show = "article.cover !== ''" :src="article.cover" alt="">
-        <div class="summary">{{article}}</div>
+        <img v-show="article.cover !== ''" :src="article.cover" alt="">
+        <div class="summary">{{article.content}}</div>
     </div>
     <div class="footer" v-if="article">
         <!-- 底部横幅内容 -->
@@ -36,12 +34,18 @@
 <script>
 export default {
     name: 'blog_card',
-    props:['article'],
-    mounted(){
+    props: ['article'],
+    mounted() {},
+    methods:{
+        to_Read(){
+            this.$router.replace({
+                path:"/read"
+            });
+        }   
     },
-    data(){
+    data() {
         return {
-            summary_max_length: 400,   //摘要最长字数
+            summary_max_length: 400, //摘要最长字数
         }
     }
 
@@ -50,73 +54,85 @@ export default {
 
 <style scoped>
 /* 封面样式 */
-.cover{
+.cover {
     background-color: blanchedalmond;
     display: flex;
     justify-content: left;
 }
-.cover > img{
+
+.cover>img {
     background-repeat: no-repeat;
     width: 70px;
     height: 70px;
 }
+
 /* 文章标题样式 */
-.title{
+.title {
     font-size: 19px;
     text-align: left;
 }
 
 /* 标签样式 */
-.el-tag{
+.el-tag {
     margin-right: 10px;
     color: black;
     border-radius: 13px;
 
 }
+
 /* 标签样式 */
-.tag{
+.tag {
     /* background-color: green; */
     text-align: left;
     padding-bottom: 7px;
 }
+
 /* 摘要样式 */
-.summary{
+.summary {
     text-align: left;
 }
+
 /* 底部内容样式 */
-.footer{
+.footer {
     padding-top: 8px;
     display: flex;
     justify-content: left;
 }
-.footer > div {
+
+.footer>div {
     margin-left: 10px;
     text-align: left;
     display: flex;
     justify-content: left;
     align-items: center;
 }
-.footer > div > img{
-    width:20px;
+
+.footer>div>img {
+    width: 20px;
     height: 20px;
     margin-right: 5px;
 }
-.footer_time{
+
+.footer_time {
     width: 20%;
     margin-left: 0px !important;
 }
-.footer_count{
+
+.footer_count {
     width: 15%;
 }
-.footer_like{
-    width: 15%;
-    margin-left: 20px;
-}
-.footer_comment{
+
+.footer_like {
     width: 15%;
     margin-left: 20px;
 }
-.fill{
+
+.footer_comment {
+    width: 15%;
+    margin-left: 20px;
+}
+
+.fill {
     width: 20%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,15 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import axios from 'axios'
+import VueRouter from 'vue-router'
+import router from './router/index.js'
 import 'element-ui/lib/theme-chalk/index.css';
 
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.use(VueRouter);
 Vue.prototype.$http = axios
-axios.defaults.baseURL = 'http://localhost:8081'
+axios.defaults.baseURL = 'http://localhost:8080'
 
 new Vue({
   axios,
+  router:router,
   render: h => h(App)
 }).$mount('#app')
